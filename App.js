b3aacc4bef910289595d702f070e99e0ab068e44
Subrat Panda <subrpan@amazon.com>
2018-05-18 00:46:40 +0530

    Integrated CodePush

--- App.js
+++ App.js
@@ -7,15 +7,20 @@ import {
   Text,
   View
 } from 'react-native';
+import codePush from "react-native-code-push";
+
 import { createStackNavigator } from 'react-navigation';
 import { WelcomePage } from './js/ui/WelcomePage';
 import { MovieListPageNative } from './js/ui/MovieListPageNative';
 import { MovieListPageRN } from './js/ui/MovieListPageRN';
 import { MovieListPageMSite } from './js/ui/MovieListPageMSite';
 
-export const App = createStackNavigator({
+const App1 = createStackNavigator({
   WelcomePage: { screen: WelcomePage },
   MovieListPageNative: { screen: MovieListPageNative },
   MovieListPageRN: { screen: MovieListPageRN },
   MovieListPageMSite: { screen: MovieListPageMSite }
 });
+
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
+export const App = codePush(codePushOptions)(App1);
